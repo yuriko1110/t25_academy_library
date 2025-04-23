9fb1f374e625d1d4343ef87f5f1f1d12e46d76e3
yuriko1110 <yuriko.ono@metateam.co.jp>
2025-04-23 19:28:44 +0900

    書籍管理システムに書籍登録機能を追加

--- src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
+++ src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
@@ -14,4 +14,7 @@ public interface BookMstRepository extends JpaRepository<BookMst, Long> {
 
 	@Query(value = "SELECT * FROM book_mst WHERE id = ?1", nativeQuery = true)
 	Optional<BookMst> selectById(Long id);
+
+	@Query(value = "SELECT * FROM book_mst WHERE isbn = ?1", nativeQuery = true)
+	Optional<BookMst> selectByIsbn(Long isbn);
 }
